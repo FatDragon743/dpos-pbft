38500a28839441ce6ec6b2e506c61381292da1fe
FatDargon <zhujmhh@qq.com>
2019-11-27 04:08:08 +0800

    Initial commit

---
+++ blockchain.js
@@ -0,0 +1,252 @@
+var assert = require('assert');
+var EventEmitter = require('events');
+var util = require('util');
+var slots = require('./slots');
+var protocol = require('./protocol');
+var Block = require('./block');
+var Transaction = require('./transaction');
+var HashList = require('./hashlist');
+var Pbft = require('./pbft');
+
+var COIN = 100000000;
+
+function BlockChain(node) {
+  EventEmitter.call(this);
+  this.node = node;
+  this.genesis = new Block({
+    height: 0,
+    timestamp: 1462953000,
+    previousHash: '',
+    generatorId: 0,
+    transactions: [
+      {
+        amount: 100000000 * COIN,
+        timestamp: 1462953000,
+        recipient: 'neo',
+        sender: '',
+      }
+    ]
+  });
+  this.pendingTransactions = {};
+
+  //txIndex[txHash] = txContent
+  this.transactionIndex = {};
+
+  this.chain = new HashList();
+  this.chain.add(this.genesis.getHash(), this.genesis);
+  this.pbft = new Pbft(this);
+  this.lastSlot = 0;
+}
+
+util.inherits(BlockChain, EventEmitter);
+
+BlockChain.prototype.start = function () {
+  var self = this;
+  setImmediate(function nextLoop() {
+    self.loop_(function () {
+      setTimeout(nextLoop, 1000);
+    });
+  });
+}
+
+// 获取 tx 的 hash，通过hash 检索，pending 和 commited 的 tx 列表，返回tx是否从来没有出现过
+BlockChain.prototype.hasTransaction = function (trs) {
+  var id = trs.getHash();
+  return !!this.pendingTransactions[id] || !!this.transactionIndex[id];
+}
+
+// 交易是否为空
+BlockChain.prototype.validateTransaction = function (trs) {
+  return !!trs;
+}
+
+// 新出现的tx 先加入 pending 队列。 
+BlockChain.prototype.addTransaction = function (trs) {
+  this.pendingTransactions[trs.getHash()] = trs;
+}
+
+// 该 block 的 hash 是否已经出现在chain上了，或者已经在 pbft 程序中
+BlockChain.prototype.hasBlock = function (hash) {
+  return !!this.chain.get(hash) || this.pbft.hasBlock(hash);
+}
+
+// 该block是否为空，且是否合法（为最后一个区块之后的顺序区块，height和hash是否延续）
+BlockChain.prototype.validateBlock = function (block) {
+  if (!block) {
+    return false;
+  }
+  var lastBlock = this.chain.last();
+  return block.getHeight() === lastBlock.getHeight() + 1 &&
+    block.getPreviousHash() === lastBlock.getHash();
+}
+
+// 添加 block 
+// 不通过 pbft 且 不是坏节点 -> 加入pbft 程序
+// 通过 pbft 或者 坏节点 -> 直接将其添加到chain上，且不忙碌
+BlockChain.prototype.addBlock = function (block) {
+  if (Flags.pbft && !this.node.isBad) {
+    var slotNumber = slots.getSlotNumber(slots.getTime(block.getTimestamp() * 1000));
+    this.pbft.addBlock(block, slotNumber);
+  } else {
+    this.commitBlock(block);
+    this.isBusy = false;
+  }
+}
+
+
+// 正式承认区块合法性，将区块加入chain，将该区块中的所有tx加入 txIndex[txHash] = txContent
+BlockChain.prototype.commitBlock = function (block) {
+  this.chain.add(block.getHash(), block);
+  var transactions = block.getTransactions();
+  for (var i in transactions) {
+    this.transactionIndex[transactions[i].getHash()] = transactions[i];
+  }
+}
+// chain 处理消息
+// 开始处理：接受到一个 tx，开始整个 共识过程
+//    tx -> new tx 如果没有 出现过 且 不为空 则 广播 tx 且 加入 pending 队列
+// 结束处理：接受到一个 block，结束共识过程，添加进入chain末尾，并广播区块（已经完成共识，所有节点完成的动作，就是接受节点并且转发）
+//    block -> new block 如果没有出现过且不为空 则广播 block 且 加入 chain
+// 其他 -> （好节点）进入pbft流程
+BlockChain.prototype.processMessage = function (msg) {
+  switch (msg.type) {
+    case protocol.MessageType.Transaction:
+      var trs = new Transaction(msg.body);
+      if (!this.hasTransaction(trs)) {
+        if (this.validateTransaction(trs)) {
+          this.node.broadcast(msg);
+          this.addTransaction(trs);
+        }
+      }
+      break;
+    case protocol.MessageType.Block:
+      var block = new Block(msg.body);
+      if (!this.hasBlock(block.getHash())) {
+        if (this.validateBlock(block)) {
+          this.node.broadcast(msg);
+          this.addBlock(block);
+        }
+      }
+      break;
+    default:
+      if (Flags.pbft && !this.node.isBad) {
+        this.pbft.processMessage(msg);
+      }
+      break;
+  }
+}
+
+/**
+* 创建 block
+* 获得最后一个block
+* 计算新区块的height，timestamp，上一个blockhash，block创建者node的ID
+* 加入所有pending 队列中的tx，清空pending
+* 返回 newblock
+*/
+BlockChain.prototype.createBlock = function (cb) {
+  var lastBlock = this.chain.last();
+  assert(!!lastBlock);
+  var newBlock = new Block({
+    height: lastBlock.getHeight() + 1,
+    timestamp: Math.floor(Date.now() / 1000),
+    previousHash: lastBlock.getHash(),
+    generatorId: this.node.id,
+  });
+  for (var k in this.pendingTransactions) {
+    newBlock.addTransaction(this.pendingTransactions[k]);
+  }
+  this.pendingTransactions = {};
+  return newBlock;
+}
+/**
+ * 打印blockchain 
+ * （第i个block:hash前6位:产生该区块的node ID）->（第i+1个block:hash前6位:产生该区块的node ID）->
+ */
+BlockChain.prototype.printBlockChain = function () {
+  var output = '';
+  this.chain.each(function (block, i) {
+    output += util.format('(%d:%s:%d) -> ', i, block.getHash().substr(0, 6), block.getGeneratorId());
+  });
+  console.log('node ' + this.node.id, output);
+}
+
+/**
+ *  制造分叉
+ * 模拟整个流程 创建block1 -> 伪造 tx1 队列[就一个tx1(cracher->alice:1000)] -> 创建block2 -> 伪造 tx2 队列[就一个tx2(cracher->bob:1000)]
+ * -> 打印 （攻击节点 ID，攻击区块高度，分叉1：block1的hash，分叉2：block2的hash）
+ * -> 将 此node存储的 通讯录中的 一般的节点发送 block1，另外一半的节点发送 block2，并打印该行为
+ * -> 此节点 采纳 block1
+ */
+BlockChain.prototype.makeFork_ = function () {
+  var lastBlock = this.chain.last();
+  assert(!!lastBlock);
+  var height = lastBlock.getHeight() + 1;
+  var timestamp = Math.floor(Date.now() / 1000);
+  var block1 = new Block({
+    height: height,
+    timestamp: timestamp,
+    previousHash: lastBlock.getHash(),
+    generatorId: this.node.id
+  });
+  block1.addTransaction(new Transaction({
+    amount: 1000,
+    recipient: 'alice',
+    sender: 'cracker'
+  }));
+  var block2 = new Block({
+    height: height,
+    timestamp: timestamp,
+    previousHash: lastBlock.getHash(),
+    generatorId: this.node.id
+  });
+  block2.addTransaction(new Transaction({
+    amount: 1000,
+    recipient: 'bob',
+    sender: 'cracker'
+  }));
+  console.log('fork on node: %d, height: %d, fork1: %s, fork2: %s', this.node.id, lastBlock.getHeight() + 1, block1.getHash(), block2.getHash());
+  var i = 0;
+  for (var id in this.node.peers) {
+    if (i++ % 2 === 0) {
+      console.log('send fork1 to', id);
+      this.node.peers[id].send(protocol.blockMessage(block1.getData()));
+    } else {
+      console.log('send fork2 to', id);
+      this.node.peers[id].send(protocol.blockMessage(block2.getData()));
+    }
+  }
+  this.addBlock(block1);
+}
+
+
+/**
+ * 
+ */
+BlockChain.prototype.loop_ = function (cb) {
+  var currentSlot = slots.getSlotNumber();
+  var lastBlock = this.chain.last();
+  assert(!!lastBlock);
+  // this.printBlockChain();
+  var lastSlot = slots.getSlotNumber(slots.getTime(lastBlock.getTimestamp() * 1000));
+  if (currentSlot === lastSlot || Date.now() % 10000 > 5000) {
+    return cb();
+  }
+  if (Flags.pbft && this.lastSlot === currentSlot) {
+    return cb();
+  }
+  var delegateId = currentSlot % slots.delegates;
+  if (this.node.id === delegateId) {
+    if (!this.node.isBad) {
+      var block = this.createBlock();
+      console.log('slot: %d, height: %d, nodeId: %d', currentSlot, block.getHeight(), this.node.id);
+      this.addBlock(block);
+      this.emit('new-message', protocol.blockMessage(block.getData()));
+      this.lastSlot = currentSlot;
+    } else {
+      this.makeFork_();
+    }
+  }
+  cb();
+}
+
+module.exports = BlockChain;
---
+++ slots.js
@@ -0,0 +1,92 @@
+/**
+ * 2016-05-11 15:50:00
+ * magic time 
+ * 系统启动时间
+ */
+function beginEpochTime() {
+  var d = new Date(1462953000000);
+
+  return d;
+}
+
+/**
+ * 当前时间-magic time
+ * 和系统启动时间差
+ * @param {*} time 
+ */
+function getEpochTime(time) {
+  if (time === undefined) {
+    time = (new Date()).getTime();
+  }
+  var d = beginEpochTime();
+  var t = d.getTime();
+  return Math.floor((time - t) / 1000);
+}
+
+module.exports = {
+/***
+ * 时间间隔 单位为s 每10进行一次
+ */
+  interval: 10,
+/**
+ * 一个20个候选人
+ */
+  delegates: 20,
+  /***
+   * 获得时间差
+   */
+  getTime: function (time) {
+    return getEpochTime(time);
+  },
+  /**
+   * 根据时间差 返回 真实时间
+   * 基准时间+时间差 返回当前时间？
+   * 返回的当前时间 除去了毫秒的值，给一个小于此值的最小整数
+   * @param {*} epochTime 
+   */
+  getRealTime: function (epochTime) {
+    if (epochTime === undefined) {
+      epochTime = this.getTime()
+    }
+    var d = beginEpochTime();
+    var t = Math.floor(d.getTime() / 1000) * 1000;
+    return t + epochTime * 1000;
+  },
+/**
+ * 用于计算时间戳偏移量，每经过10s（interval），其结果加1，对其结果取20的余数就可以获得出票节点编号。
+ * 时间差（一代的时间） / 间隔
+ * @param {*} epochTime 
+ */
+  getSlotNumber: function (epochTime) {
+    if (epochTime === undefined) {
+      epochTime = this.getTime()
+    }
+    return Math.floor(epochTime / this.interval);
+  },
+
+  /**
+   * 根据时间戳偏移量 反向计算时间差
+   * @param {*} slot 
+   */
+  getSlotTime: function (slot) {
+    return slot * this.interval;
+  },
+/**
+ * 获得下一个时间戳偏移量
+ */
+  getNextSlot: function () {
+    var slot = this.getSlotNumber();
+
+    return slot + 1;
+  },
+/***
+ * 获得上一个时间错偏移量
+ */
+  getLastSlot: function (nextSlot) {
+    return nextSlot + this.delegates;
+  },
+
+  roundTime: function (date) {
+    Math.floor(date.getTime() / 1000) * 1000
+  }
+}
